8954863fb84ba5dda49d9464eb5c02b170f73798
elhadj hamlaoui <hamlaouielhadj@gmail.com>
2020-04-30 16:26:11 +0000

    test

--- backup/app.js
+++ backup/app.js
@@ -55,7 +55,7 @@ const initializeBrowser = async () => {
     pageBreaking.on('error', (err) => {
       console.log('error happen at the page: ', err)
     })
-    page.on('requestfailed', (err) => {console.error('REQUEST_FAILED:\n'); console.error(err)})
+    page.on('requestfailed', (err) => console.error('REQUEST_FAILED:\n' + err))
 
     pageBreaking.on('pageerror', function (err) {
       console.log('Page2 error: ' + err)
